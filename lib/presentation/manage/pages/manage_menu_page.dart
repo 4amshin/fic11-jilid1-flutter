a5203d41f730d57c5ecb6cb0c48a82e54815aa70
4amshin <shinmendenz@gmail.com>
2024-09-07 09:40:47 +0800

    Tab Menu Basic UI

--- lib/presentation/manage/pages/manage_menu_page.dart
+++ lib/presentation/manage/pages/manage_menu_page.dart
@@ -1,3 +1,8 @@
+import 'package:fic11_jilid1/core/assets/assets.gen.dart';
+import 'package:fic11_jilid1/core/components/menu_button.dart';
+import 'package:fic11_jilid1/core/components/spaces.dart';
+import 'package:fic11_jilid1/core/extensions/build_context_ext.dart';
+import 'package:fic11_jilid1/presentation/manage/pages/manage_printer_page.dart';
 import 'package:flutter/material.dart';
 
 class ManageMenuPage extends StatelessWidget {
@@ -9,6 +14,30 @@ class ManageMenuPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Kelola'),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                MenuButton(
+                  iconPath: Assets.images.manageProduct.path,
+                  label: 'Kelola Produk',
+                  onPressed: () {},
+                  isImage: true,
+                ),
+                const SpaceWidth(15.0),
+                MenuButton(
+                  iconPath: Assets.images.managePrinter.path,
+                  label: 'Kelola Printer',
+                  onPressed: () => context.push(const ManagePrinterPage()),
+                  isImage: true,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/order/pages/orders_page.dart
+++ lib/presentation/order/pages/orders_page.dart
@@ -1,4 +1,11 @@
 import 'package:fic11_jilid1/core/assets/assets.gen.dart';
+import 'package:fic11_jilid1/core/components/menu_button.dart';
+import 'package:fic11_jilid1/core/components/spaces.dart';
+import 'package:fic11_jilid1/presentation/order/models/order_model.dart';
+import 'package:fic11_jilid1/presentation/order/widgets/order_card.dart';
+import 'package:fic11_jilid1/presentation/order/widgets/payment_cash_dialog.dart';
+import 'package:fic11_jilid1/presentation/order/widgets/payment_qris_dialog.dart';
+import 'package:fic11_jilid1/presentation/order/widgets/process_button.dart';
 import 'package:flutter/material.dart';
 
 class OrdersPage extends StatelessWidget {
@@ -6,6 +13,29 @@ class OrdersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final indexValue = ValueNotifier(0);
+    const paddingHorizontal = EdgeInsets.symmetric(horizontal: 16.0);
+    final List<OrderModel> orders = [
+      OrderModel(
+        image: Assets.images.f1.path,
+        name: 'Nutty Oat Latte',
+        price: 39000,
+      ),
+      OrderModel(
+        image: Assets.images.f2.path,
+        name: 'Iced Latte',
+        price: 24000,
+      ),
+    ];
+
+    int calculateTotalPrice(List<OrderModel> orders) {
+      int totalPrice = 0;
+      for (final order in orders) {
+        totalPrice += order.price;
+      }
+      return totalPrice;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Order Detail'),
@@ -16,6 +46,72 @@ class OrdersPage extends StatelessWidget {
             icon: Assets.icons.delete.svg(),
           ),
         ],
+      ),
+      body: StatefulBuilder(
+        builder: (context, setState) => ListView.separated(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          itemCount: orders.length,
+          separatorBuilder: (context, index) => const SpaceHeight(20.0),
+          itemBuilder: (context, index) => OrderCard(
+            padding: paddingHorizontal,
+            data: orders[index],
+            onDeleteTap: () {
+              orders.removeAt(index);
+              setState(() {});
+            },
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ValueListenableBuilder(
+              valueListenable: indexValue,
+              builder: (context, value, _) => Row(
+                children: [
+                  const SpaceWidth(10.0),
+                  MenuButton(
+                    iconPath: Assets.icons.cash.path,
+                    label: 'Tunai',
+                    isActive: value == 1,
+                    onPressed: () => indexValue.value = 1,
+                  ),
+                  const SpaceWidth(10.0),
+                  MenuButton(
+                    iconPath: Assets.icons.qrCode.path,
+                    label: 'QRIS',
+                    isActive: value == 2,
+                    onPressed: () => indexValue.value = 2,
+                  ),
+                  const SpaceWidth(10.0),
+                ],
+              ),
+            ),
+            const SpaceHeight(20.0),
+            ProcessButton(
+              price: 123000,
+              onPressed: () async {
+                if (indexValue.value == 0) {
+                } else if (indexValue.value == 1) {
+                  showDialog(
+                    context: context,
+                    builder: (context) => PaymentCashDialog(
+                      price: calculateTotalPrice(orders),
+                    ),
+                  );
+                } else if (indexValue.value == 2) {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (context) => const PaymentQrisDialog(),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
